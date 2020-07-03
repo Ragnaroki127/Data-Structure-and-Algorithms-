a9fed70a6adf9151a2e787d8a1bede9baf8d68ce
Haoxin Yan <yanhx19@mails.tsinghua.edu.cn>
2020-07-03 11:40:52 +0200

    Finished Advanced Algorithms Assignments

--- Advanced Algorithms and Complexity/Week4/Programming-Assignment-4/school_bus/school_bus.py
+++ Advanced Algorithms and Complexity/Week4/Programming-Assignment-4/school_bus/school_bus.py
@@ -1,5 +1,7 @@
 # python3
 from itertools import permutations
+from itertools import combinations
+
 INF = 10 ** 9
 
 def read_data():
@@ -12,13 +14,13 @@ def read_data():
         graph[u][v] = graph[v][u] = weight
     return graph
 
-def print_answer(path_weight, path):
-    print(path_weight)
-    if path_weight == -1:
+def print_answer( weight_of_path, path ):
+    print(weight_of_path)
+    if weight_of_path == -1:
         return
     print(' '.join(map(str, path)))
 
-def optimal_path(graph):
+def optimal_path_bf( graph ):
     # This solution tries all the possible sequences of stops.
     # It is too slow to pass the problem.
     # Implement a more efficient algorithm here.
@@ -44,6 +46,36 @@ def optimal_path(graph):
         return (-1, [])
     return (best_ans, [x + 1 for x in best_path])
 
+
+def optimal_path( graph ):
+    n = len(graph)
+    C = [[INF for _ in range(n)] for __ in range(1 << n)]
+    backtrack = [[(-1, -1) for _ in range(n)] for __ in range(1 << n)]
+    C[1][0] = 0
+    for size in range(1, n):
+        for S in combinations(range(n), size):
+            S = (0,) + S
+            k = sum([1 << i for i in S])
+            for i in S:
+                if 0 != i:
+                    for j in S:
+                        if j != i:
+                            curr = C[k ^ (1 << i)][j] + graph[i][j]
+                            if curr < C[k][i]:
+                                C[k][i] = curr
+                                backtrack[k][i] = (k ^ (1 << i), j)
+
+    best_result, currIndex2 = min([(C[(1 << n) - 1][i] + graph[0][i], i) for i in range(n)])
+
+    if best_result >= INF:
+        return (-1, [])
+
+    bestPath = []
+    currIndex1 = (1 << n) - 1
+    while -1 != currIndex1:
+        bestPath.insert(0, currIndex2 + 1)
+        currIndex1, currIndex2 = backtrack[currIndex1][currIndex2]
+    return (best_result, bestPath)
 
 if __name__ == '__main__':
     print_answer(*optimal_path(read_data()))
